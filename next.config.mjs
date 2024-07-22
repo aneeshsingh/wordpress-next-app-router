aae0c6efb795d0ba3abceb98e532fb48ad0e2423
aneeshsingh <aneesh@alkye.com>
2024-07-22 07:17:57 +0100

    First Push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '34.170.167.73',
+            },
+            {
+                protocol: 'http',
+                hostname: '2.gravatar.com',
+            },
+        ],
+    },
+};
 export default nextConfig;
